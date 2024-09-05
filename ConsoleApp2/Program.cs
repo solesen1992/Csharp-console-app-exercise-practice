563e9b4f899d47520f38d06c4b82afffd010587f
solesen1992 <saramundal@live.dk>
2024-09-05 09:55:20 +0200

    Creating exercises

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2;
+
+internal class Program {
+
+    /**
+     * Another way of doing the main entrance of the program.
+     * 
+     * Ctrl + c + k = make it into comments
+     * Ctrl + u = undo comments
+     * Ctrl + k + g = The } gets into the right place. Make things pretty.
+     */
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Full/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Full.Model;
+
+namespace Full {
+    class FullProgram { 
+        static void Main()
+        {
+            FullPerson aPerson = new FullPerson();
+
+            aPerson.FirstName = " ";
+            aPerson.LastName = "Hansen";
+            DateTime aBirthDate = new DateTime(1992, 11, 20);
+            aPerson.DateOfBirth = aBirthDate;
+
+            Console.WriteLine(aPerson);
+        }
+    }
+}
