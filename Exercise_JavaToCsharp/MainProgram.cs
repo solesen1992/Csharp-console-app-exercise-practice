3535430a4995401f08a560125ab86dd77eebb6e4
solesen1992 <saramundal@live.dk>
2024-09-07 22:17:12 +0200

    Changes added

---
+++ Exercise_JavaToCsharp/MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using Exercise_JavaToCsharp.Model;
+
+class MainProgram
+{
+
+    static void Main()
+    {
+
+        Person p1 = new Person("Hans Hansen", 1990);
+        Console.WriteLine(p1.ToString());
+    }
+}
---
+++ Exercise_JavaToCsharp/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_JavaToCsharp.Model
+{
+    internal class Person
+    {
+        private String name;
+        private int yearOfBirth;
+
+        public Person(String name, int yearOfBirth)
+        {
+            this.name = name;
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        public override string? ToString()
+        {
+            return $"{name} born {yearOfBirth}";
+        }
+    }
+}
