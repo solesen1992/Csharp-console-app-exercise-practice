c1eca7e5faa41e791369d880052ff6e47a04093a
solesen1992 <saramundal@live.dk>
2024-09-26 11:09:18 +0200

    Create Exercise_Async1

---
+++ Exercise_Async1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+
+const int repetitions = 1000;
+
+// UseSystemThreading();
+// UseSystemThreadingTasks();
+UseAsync();
+
+static void UseSystemThreading()
+{
+    // Create thread and associate method
+    ThreadStart threadStart = DoWork;    // ThreadStart Delegate
+    Thread plusThread = new Thread(threadStart);
+    // Start thread
+    plusThread.Start();
+    //
+    for (int count = 0; count < repetitions; count++)
+    {
+        Console.Write('-');
+    }
+    // Wait for plusThread to terminate
+    plusThread.Join();
+}
+
+static void DoWork()
+{
+    for (int count = 0; count < repetitions; count++)
+    {
+        Console.Write('+');
+    }
+}
+
+static void UseSystemThreadingTasks()
+{
+    // Create and run Task
+    Task plusTask = Task.Run(
+        () => {
+            for (int count = 0; count < repetitions; count++)
+            {
+                Console.Write('+');
+            }
+
+        }
+    );
+
+    Debug.WriteLine("Plus Task status: " + plusTask.Status);
+
+    for (int count = 0; count < repetitions; count++)
+    {
+        Console.Write('-');
+    }
+
+    plusTask.Wait();
+    Debug.WriteLine("Plus Task status: " + plusTask.Status);
+}
+
+// The job of the UseAsync method is to display minuses while the job of the DoWorkAsync method is to display plusses.
+// Minusses
+static async void UseAsync()
+{
+    // 1. Call DoWorkAsync()
+    var doWorkTask = DoWorkAsync();
+    // 2. Print minuses
+    for (int count = 0; count < repetitions; count++)
+    {
+        Console.Write('-');
+    }
+
+    // Wait for the async work to complete
+    await doWorkTask;
+}
+
+// Plusses
+static async Task DoWorkAsync()
+{
+    // Start code:
+    await Task.Run(() => {
+        for (int count = 0; count < repetitions; count++)
+        {
+            Console.Write('+');
+        }
+    });
+}
