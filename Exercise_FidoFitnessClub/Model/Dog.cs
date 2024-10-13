4e389c1182bb3bff6b6dfdaa152e2a8ac34492da
solesen1992 <saramundal@live.dk>
2024-10-13 14:33:03 +0200

    Transform FidoFitnessClub from Java to C#

---
+++ Exercise_FidoFitnessClub/Model/Dog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_FidoFitnessClub.Model
+{
+    public class Dog
+    {
+        // Properties
+        public string Name { get; set; }
+        public int FeeYear { get; set; }
+
+        // Constructor
+        public Dog(string name, int feeYear)
+        {
+            Name = name;
+            FeeYear = feeYear;
+        }
+
+        public Dog(string name) {
+            Name = name;
+        }
+
+        public Boolean isFeeDue(int year) {
+            return year > FeeYear;
+            /*
+             * boolean result = false;
+             * if (year > this.feeYear) {
+             *  result = true;
+             * }
+             * return result;
+             */
+        }
+
+        public int dueYearsCount(int year) {
+            return year - FeeYear;
+            /*
+             * int result = year - feeYear;
+             * return result;
+             */
+        }
+
+        //To string
+        public override string ToString()
+        {
+            return $"Dog {Name}, feeYear = {FeeYear}";
+        }
+
+    }
+}
